c7a757d45d30c212e7af25037adc2c1913c57335
Zhekich123 <axelb380@gmail.com>
2023-09-15 12:13:02 +0300

    adding dist

---
+++ dist/index.a61023f1.js
@@ -0,0 +1,3 @@
+const e=document.querySelector(".choosed-language"),t=document.querySelectorAll(".dropdown-item--language");function n(t){let n=t.target.dataset.value;e.innerHTML=n}t.forEach(function(e){e.addEventListener("click",n)});//# sourceMappingURL=index.a61023f1.js.map
+
+//# sourceMappingURL=index.a61023f1.js.map
